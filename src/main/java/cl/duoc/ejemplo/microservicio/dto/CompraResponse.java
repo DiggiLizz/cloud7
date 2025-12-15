62a91c13f630d0dbccca01974cec0af683f100e0
Lilian Zapata <lili.zapata@duocuc.c>
2025-12-15 07:22:59 -0300

    cambio dto

--- src/main/java/cl/duoc/ejemplo/microservicio/dto/CompraResponse.java
+++ src/main/java/cl/duoc/ejemplo/microservicio/dto/CompraResponse.java
@@ -14,6 +14,8 @@ public class CompraResponse {
     private BigDecimal precioTotal;
     private LocalDateTime fechaCompra;
 
+    public CompraResponse() {}
+
     public CompraResponse(Long compraId, Long eventoId, String nombreEvento, LocalDate fechaEvento,
                             Integer cantidad, BigDecimal precioUnitario, BigDecimal precioTotal,
                             LocalDateTime fechaCompra) {
@@ -26,6 +28,16 @@ public class CompraResponse {
         this.precioTotal = precioTotal;
         this.fechaCompra = fechaCompra;
     }
+
+    //setters y getters
+    public void setCompraId(Long compraId) { this.compraId = compraId; }
+    public void setEventoId(Long eventoId) { this.eventoId = eventoId; }
+    public void setNombreEvento(String nombreEvento) { this.nombreEvento = nombreEvento; }
+    public void setFechaEvento(LocalDate fechaEvento) { this.fechaEvento = fechaEvento; }
+    public void setCantidad(Integer cantidad) { this.cantidad = cantidad; }
+    public void setPrecioUnitario(BigDecimal precioUnitario) { this.precioUnitario = precioUnitario; }
+    public void setPrecioTotal(BigDecimal precioTotal) { this.precioTotal = precioTotal; }
+    public void setFechaCompra(LocalDateTime fechaCompra) { this.fechaCompra = fechaCompra; }
 
     public Long getCompraId() { return compraId; }
     public Long getEventoId() { return eventoId; }
